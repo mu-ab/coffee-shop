e49598233ce4faad9e490f178e952370c331a082
Ahmed Musa <musaahmed338@gmail.com>
2022-03-03 21:55:49 +0100

    Added pages collection

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,13 @@ module.exports = {
         path: "src/blog",
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pageData",
+        path: "src/pageData",
+      },
+    },
     "gatsby-transformer-remark",
   ],
 }
